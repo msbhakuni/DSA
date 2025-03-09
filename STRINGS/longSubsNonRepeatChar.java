095b45dd720d37fa33e1994781162bc3010b69d0
MANEESH SINGH BHAKUNI <msbhakuni@MANEESHs-MacBook-Air.local>
2025-03-09 23:43:38 +0530

    longest substring without repeating characters

---
+++ STRINGS/longSubsNonRepeatChar.java
@@ -0,0 +1,24 @@
+import java.util.*;
+//Longest substring without repeating characters
+
+class longSubsNonRepeatChar {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your string : ");
+        String str = scanner.nextLine();
+
+        int left = 0;
+        int max_len = 0;
+        Map<Character,Integer> map = new HashMap<>();
+        for(int right=0;right<str.length();right++){
+            char c = str.charAt(right);
+            if(map.containsKey(c)){
+                left = map.get(c)+1;
+            }
+            map.put(c,right);
+            max_len = Math.max(max_len,right-left+1);
+        }
+
+        System.out.println("ans is : "+max_len);
+    }
+}
